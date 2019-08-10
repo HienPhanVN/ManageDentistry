73af9af74a27f946e7099e25bfb3730140ed0c44
Hien <hienphanprivate@gmail.com>
2019-08-10 19:07:11 +0700

    PVDHien init Project

---
+++ Thesis/TemplateMVC/TemplateMVC/Controllers/DBUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace TemplateMVC.Controllers
+{
+    public class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            
+            string host = "127.0.0.1";
+            int port = 3306;
+            string database = "sakila";
+            string username = "admin";
+            string password = "admin";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
---
+++ Thesis/TemplateMVC/TemplateMVC/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace TemplateMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            try
+            {
+                conn.Open();
+                String sql = "SELECT * FROM sakila.actor;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (DbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+
+                        while (reader.Read())
+                        {
+                            // Chỉ số (index) của cột Emp_ID trong câu lệnh SQL.
+                            int empIdIndex = reader.GetOrdinal("actor_id"); // 0
+
+
+                            long empId = Convert.ToInt64(reader.GetValue(0));
+
+                            // Cột Emp_No có index = 1.
+                            string empNo = reader.GetString(1);
+                            int empNameIndex = reader.GetOrdinal("last_name");// 2
+                            string empName = reader.GetString(empNameIndex);
+
+                            // Chỉ số (index) của cột Mng_Id trong câu lệnh SQL.
+                            int mngIdIndex = reader.GetOrdinal("actor_id");
+
+                            long? mngId = null;
+
+                            // Kiểm tra giá trị của cột này có thể null hay không.
+                            if (!reader.IsDBNull(mngIdIndex))
+                            {
+                                mngId = Convert.ToInt64(reader.GetValue(mngIdIndex));
+                            }
+                            string[] proinfo = new string[] { "a", "b" };
+                            ViewBag.ProInfo = proinfo;
+                            ViewBag.TotalStudents = empName;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                // Đóng kết nối.
+                conn.Close();
+                // Tiêu hủy đối tượng, giải phóng tài nguyên.
+                conn.Dispose();
+            }   
+            return View();
+        }
+        public bool AddEmployee(EmpModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("AddNewEmpDetails", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+        
+    
+    }   //end class
+}       //end namespace
