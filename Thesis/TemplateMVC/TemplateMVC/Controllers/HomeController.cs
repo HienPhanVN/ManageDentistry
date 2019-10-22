cbd8271f052888bc5ccb8222097b420967214ade
Hien <hienphanprivate@gmail.com>
2019-10-22 16:09:58 +0700

    PVDHien complete user functions

--- Thesis/TemplateMVC/TemplateMVC/Controllers/HomeController.cs
+++ Thesis/TemplateMVC/TemplateMVC/Controllers/HomeController.cs
@@ -16,66 +16,6 @@ namespace TemplateMVC.Controllers
     public class HomeController : Controller
     {
         
-
-        // GET: Home
-        public ActionResult Index()
-        {
-            MySqlConnection conn = DBUtils.GetDBConnection();
-            try
-            {
-                conn.Open();
-                String sql = "SELECT * FROM sakila.actor;";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                using (DbDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-
-                        while (reader.Read())
-                        {
-                            // Chỉ số (index) của cột Emp_ID trong câu lệnh SQL.
-                            int empIdIndex = reader.GetOrdinal("actor_id"); // 0
-
-
-                            long empId = Convert.ToInt64(reader.GetValue(0));
-
-                            // Cột Emp_No có index = 1.
-                            string empNo = reader.GetString(1);
-                            int empNameIndex = reader.GetOrdinal("last_name");// 2
-                            string empName = reader.GetString(empNameIndex);
-
-                            // Chỉ số (index) của cột Mng_Id trong câu lệnh SQL.
-                            int mngIdIndex = reader.GetOrdinal("actor_id");
-
-                            long? mngId = null;
-
-                            // Kiểm tra giá trị của cột này có thể null hay không.
-                            if (!reader.IsDBNull(mngIdIndex))
-                            {
-                                mngId = Convert.ToInt64(reader.GetValue(mngIdIndex));
-                            }
-                            string[] proinfo = new string[] { "a", "b" };
-                            ViewBag.ProInfo = proinfo;
-                            ViewBag.TotalStudents = empName;
-                        }
-                    }
-                }
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                // Đóng kết nối.
-                conn.Close();
-                // Tiêu hủy đối tượng, giải phóng tài nguyên.
-                conn.Dispose();
-            }   
-            return View();
-        }
-
         public ActionResult managepatient() 
         {
             return View();
@@ -108,9 +48,7 @@ namespace TemplateMVC.Controllers
             user patient = new user();
             bool result = patient.Create(name_user, phone_user, address_user, email_user, id_tier);
             return result;
-        }
-
-        
+        }        
     
     }   //end class
 }       //end namespace
